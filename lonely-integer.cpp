4f9c292af82de0b76481bf8f18edb78ffcc0d293
Khaled Sami <k.sami.mohammed@gmail.com>
2016-05-22 20:15:35 +0200

    Solution for lonely-integer

---
+++ lonely-integer.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int lonelyinteger(vector <int> a) {
+    int missing_number = a[0];
+    for (int i = 1; i < a.size(); i++) {
+        missing_number^= a[i];
+    }
+    return missing_number;
+}
+int main() {
+    int res;
+    
+    int _a_size;
+    cin >> _a_size;
+    cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n'); 
+    vector<int> _a;
+    int _a_item;
+    for(int _a_i=0; _a_i<_a_size; _a_i++) {
+        cin >> _a_item;
+        _a.push_back(_a_item);
+    }
+    
+    res = lonelyinteger(_a);
+    cout << res;
+    
+    return 0;
+}
+//https://www.hackerrank.com/challenges/lonely-integer
